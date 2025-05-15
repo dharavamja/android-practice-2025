10549a1812c700566bf6ec38ba0b7f10e0b8f98e
dharavamja <dharavamja@gmail.com>
2025-05-15 00:46:01 -0400

    Added reset button

--- app/src/main/java/com/example/singlescreengameapp/MainActivity.kt
+++ app/src/main/java/com/example/singlescreengameapp/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.example.singlescreengameapp
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -25,10 +25,13 @@ import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -37,6 +40,7 @@ import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontVariation.width
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -64,6 +68,8 @@ val images = listOf(
  * TASK 1 : Display each image exactly twice in a grid, on each run, in a random order.
  * TASK 2 : Match the images when clicked.If matched, green, not matched, Blue. If selected, yellow.
  * TASK 3 : Add Turn count.
+ * TASK 4 : Add Reset button.
+ * TASK 5 : Add undo button.
  */
 
 // You can update with your own data structure if needed.
@@ -90,59 +96,91 @@ private fun ContentView() {
     /**
      *  Create a full list of card.
      */
-    val cards = remember {
-        (images + images).shuffled().mapIndexed { index, imageVector ->
+    val originalCards = remember {
+        (images + images).mapIndexed { index, imageVector ->
             CardData(id = index, imageVector = imageVector)
         }.toMutableStateList()
     }
+    val cards = remember { mutableStateListOf<CardData>() }
     var firstSelectedIndex by remember { mutableStateOf<Int?>(null) }
     var turnCount by remember { mutableIntStateOf(0) }
+
+    LaunchedEffect(Unit) {
+        cards.clear()
+        cards.addAll(originalCards)
+    }
+
     Column {
-        Text(
-            text = "Turn : $turnCount",
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.DarkGray
-        )
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(3),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            contentPadding = PaddingValues(8.dp),
-        ) {
-            items(12) { index ->
-                val card = cards[index]
-                CardButton(
-                    imageVector = card.imageVector, backgroundColor = when {
-                        card.isMatched -> ColorGreen
-                        card.isSelected -> ColorYellow
-                        else -> ColorBlue
-                    }, onClick = {
-                        if (card.isMatched || card.isSelected) {
-                            //Matched items and Already selected items can't get selected
-                            return@CardButton
-                        }
-                        card.isSelected = true
-                        if (firstSelectedIndex == null) {
-                            //If first cars is selected, add it here!
-                            firstSelectedIndex = index
-                        } else {
-                            val firstSelectedCard = cards[firstSelectedIndex!!]
-                            if (firstSelectedCard.imageVector == card.imageVector) {
-                                card.isMatched = true
-                                firstSelectedCard.isMatched = true
-                            } else {
-                                card.isSelected = false
-                                firstSelectedCard.isSelected = false
-                            }
-                            turnCount++
-                            firstSelectedIndex = null
-                        }
+        Row {
+            Text(
+                text = "Turn : $turnCount",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .weight(1f),
+                textAlign = TextAlign.Center,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.DarkGray
+            )
+            FilledTonalButton(
+                onClick = {
+                    cards.clear()
+                    cards.addAll(originalCards.shuffled().map {
+                        CardData(id = it.id, imageVector = it.imageVector) // create a fresh copy
                     })
+                    firstSelectedIndex = null
+                    turnCount = 0
+                },
+                shape = RoundedCornerShape(12.dp),
+                colors = ButtonDefaults.filledTonalButtonColors(
+                    containerColor = Color(0xFFFFCDD2),
+                    contentColor = Color.Red
+                ),
+                modifier = Modifier.padding(4.dp)) {
+                Text(
+                    text = "Reset",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+        if (cards.size == 12) {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(3),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                contentPadding = PaddingValues(8.dp),
+            ) {
+                items(cards.size) { index ->
+                    val card = cards[index]
+                    CardButton(
+                        imageVector = card.imageVector, backgroundColor = when {
+                            card.isMatched -> ColorGreen
+                            card.isSelected -> ColorYellow
+                            else -> ColorBlue
+                        }, onClick = {
+                            if (card.isMatched || card.isSelected) {
+                                //Matched items and Already selected items can't get selected
+                                return@CardButton
+                            }
+                            card.isSelected = true
+                            if (firstSelectedIndex == null) {
+                                //If first cars is selected, add it here!
+                                firstSelectedIndex = index
+                            } else {
+                                val firstSelectedCard = cards[firstSelectedIndex!!]
+                                if (firstSelectedCard.imageVector == card.imageVector) {
+                                    card.isMatched = true
+                                    firstSelectedCard.isMatched = true
+                                } else {
+                                    card.isSelected = false
+                                    firstSelectedCard.isSelected = false
+                                }
+                                turnCount++
+                                firstSelectedIndex = null
+                            }
+                        })
+                }
             }
         }
     }
